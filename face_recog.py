cdb35dd69f792a3d285a958a0f3ecfecf3e55186
Siddhant96 <khandelwal.siddhant0909@gmail.com>
2017-11-30 02:22:38 +0530

    encryption added

--- face_recog.py
+++ face_recog.py
@@ -6,7 +6,11 @@ Created on Tue Nov 14 19:20:15 2017
 """
 import cv2
 import numpy as np
-
+import os
+import shutil
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
 
 def face_extractor(img):
     face_classifier = cv2.CascadeClassifier('C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml')
@@ -20,34 +24,81 @@ def face_extractor(img):
         
     return cropped_face
 
-#Initialise webcam
-cap = cv2.VideoCapture(0)
-count = 0
-#x = input()
-#Collect 100 samples of user's face from webcam
-while True:
+
+def encrypt(key, filename):
+    chunksize = 64 * 1024
+    outputFile =  './data/'+ filename
+    filename = './faces/' + filename
+    filesize = str(os.path.getsize(filename)).zfill(16)
+    IV = Random.new().read(16)
+
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+    with open(filename, 'rb') as infile:
+        with open(outputFile, 'wb') as outfile:
+            outfile.write(filesize.encode('utf-8'))
+            outfile.write(IV)
+
+            while True:
+                chunk = infile.read(chunksize)
+
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += b' ' * (16 - (len(chunk) % 16))
+
+                outfile.write(encryptor.encrypt(chunk))
+
+
+
+if __name__ == '__main__':
+    #Making the key
+    key = str('2486')
+    hasher = SHA256.new(key.encode('utf-8'))
+    key = hasher.digest()
     
-    ret,frame  = cap.read()
-    face = face_extractor(frame)
-    if face is not None:
-        count += 1
-        face_edited = cv2.cvtColor(face,cv2.COLOR_BGR2GRAY)
-        face_edited = cv2.equalizeHist(face_edited)
-        face_resized = cv2.resize(face_edited, (200,200))
-    
-        #SAVE IMAGE IN DIRECTORY
-        file_path = 'C:\\Users\\Siddhant\\jupyter_notebook\\face_detection\\faces\\' + str(count) + '.jpg'
-        cv2.imwrite(file_path,face_resized)
-        
-        cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-        cv2.imshow('Face detector',face)
-        
-    else:
-        print("Face not found")
-        pass
-    if (cv2.waitKey(13) == 13 or count == 200):
-        break
-        
-cap.release()
-cv2.destroyAllWindows()
-print ("Samples collected")
+    #Initialise webcam
+    cap = cv2.VideoCapture(0)
+    count = 0
+    try:
+        if not os.path.exists('data'):
+            os.mkdir('data')
+        if not os.path.exists('faces'):
+            os.mkdir('faces')
+    except OSError:
+        print('Error: creating directory data or faces')       
+   
+    while True:
+        ret,frame  = cap.read()
+        face = face_extractor(frame)
+        if face is not None:
+            count += 1
+            face_edited = cv2.cvtColor(face,cv2.COLOR_BGR2GRAY)
+            face_edited = cv2.equalizeHist(face_edited)
+            face_resized = cv2.resize(face_edited, (300,300))
+            
+            #SAVE IMAGE IN DIRECTORY
+            file_path = './faces/' + str(count) + '.jpg'
+            cv2.imwrite(file_path,face_resized)
+            file_path = str(count) + '.jpg'
+            encrypt(key, file_path)
+            
+            cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.imshow('Face detector',face)
+            
+        else:
+            print("Face not found")
+            pass
+        if (cv2.waitKey(13) == 13 or count == 30):
+            break
+           
+    cap.release()
+    cv2.destroyAllWindows()        
+    '''
+    try:
+        shutil.rmtree('faces', ignore_errors=True)
+    except OSError:
+        print('Error: Deleting data')
+    '''
+    #os.mkdir('faces')
+    print ("Samples collected")
--- face_recog_train.py
+++ face_recog_train.py
@@ -10,7 +10,7 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 
-data_path = 'C:\\Users\\Siddhant\\jupyter_notebook\\face_detection\\faces\\'
+data_path = 'C:\\Users\\Siddhant\\version-control\\Face-Recognition\\faces\\'
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f)) ]
 
 Training_Data, Labels = [], []
@@ -21,7 +21,7 @@ for i, files in enumerate(onlyfiles):
     image_path = data_path + onlyfiles[i]
     images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     Training_Data.append(np.asarray(images, dtype = np.uint8))
-    Labels.append(i)
+    Labels.append(onlyfiles[i].split('.')[0])
     
 Labels = np.asarray(Labels, dtype = np.int32)
 
